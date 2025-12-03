a1c55bf4b86d7f42be17782a0f95ac6286019080
Roger Terrill <lpaddikt@gmail.com>
2025-12-02 22:48:55 -0800

    Link

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,10 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
-    <main> Hello World</main>
-  );
+    <main>
+      <h1>Hello World</h1>
+      <Link href="/users">Users Page</Link>
+    </main>
+  )
 }
